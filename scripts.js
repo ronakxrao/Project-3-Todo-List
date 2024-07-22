becfec8d01a78405cd4a1ff3cbf0c8bcb4fd6d20
ronakxrao <yadavronak301@gmail.com>
2024-07-22 13:17:55 +0530

    Formatted the document

--- scripts.js
+++ scripts.js
@@ -1,37 +1,37 @@
 
 var todos = [
     {
-        text:'Learn HTML, CSS and Javascript',
+        text: 'Learn HTML, CSS and Javascript',
         done: true
     },
     {
-        text:'Learn the basics of Vue JS',
+        text: 'Learn the basics of Vue JS',
         done: false
     },
-    
+
 ];
 
-const todosApp ={
-      data() {
+const todosApp = {
+    data() {
         return {
-        todos: window.todos,
-        newTodo: {
-            done: false
+            todos: window.todos,
+            newTodo: {
+                done: false
+            }
         }
-        }
-      },
-      methods: {
-        addTodo: function() {
-            if(this.newTodo.text) {
+    },
+    methods: {
+        addTodo: function () {
+            if (this.newTodo.text) {
                 this.todos.push(this.newTodo);
-                this.newTodo ={     
-                     done: false
+                this.newTodo = {
+                    done: false
                 };
             } else {
                 alert("To-do text is required");
             }
         }
-      }
+    }
 };
 
 Vue.createApp(todosApp).mount('#app');
