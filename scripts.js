c94c21d9d5007034d0d27e6027f92c7cd503e724
ronakxrao <yadavronak301@gmail.com>
2024-07-22 11:58:35 +0530

    Added a dummy datz
    clear

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 
+var todos = [
+    {
+        text:'Learn HTML, CSS and Javascript',
+        done: true
+    },
+    {
+        text:'Learn the basics of Vue JS',
+        done: false
+    },
+    
+];
+
 const todosApp ={
       data() {
         return {
-
+        todos: window.todos
         }
       }
 };
